3932d54f46c5943438d3067fede658821b812011
Emerson Cloud <emerson.cloud@gmail.com>
2022-11-03 13:36:22 -0500

    just say duh or no duh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,15 @@
 <script>
-	let answer = '';
-	let sayit = '';
-
-	function duhOrNoDuh() {
-		answer = Math.random() < 0.5 ? 'duh' : 'no duh';
-	}
-	$: {
-		answer;
-		sayit;
-	}
+	let sayit = Math.random() < 0.5 ? 'duh' : 'noduh';
 </script>
 
-<div class="everything">
-	<input bind:value={sayit} type="text" id="sayit" size="20" placeholder="Say it" />
-	<button disabled={!sayit} on:click={duhOrNoDuh}>Should I say duh or no duh</button>
-	{#if answer && sayit}
-		<div class="answer">
-			<div class="you">'{sayit}'</div>
-			<div class="me">'{answer}'</div>
-		</div>
-	{/if}
+<div>
+	{sayit}
 </div>
 
 <style>
-	* {
-		font-family: 'Times New Roman', Times, serif;
-		font-weight: bold;
-	}
-
-	.you {
-		font-weight: normal;
-	}
-
-	.me {
-		padding-top: 1vh;
-	}
-
-	.answer {
-		padding: 1vh;
-		font-size: 1.5em;
-		position: absolute;
-	}
-
-	.everything {
-		padding: 1vh;
-	}
-
-	input {
-		font-size: 1.5em;
-		font-weight: normal;
-	}
-
-	button {
-		height: 3vh;
-		width: 100px;
-		margin-left: 1vw;
-		position: absolute;
-		font-weight: normal;
+	div {
+		font-size: 5rem;
+		text-align: center;
+		padding-top: 2.5rem;
 	}
 </style>
